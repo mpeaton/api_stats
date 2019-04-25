a9125619c71a614418f4c3ffee0fa4e9791255ad
Michael Eaton <mpeaton@amdgtechnologies.com>
2019-04-24 21:39:59 -0400

    fix visitor formatting

--- api_stats/numpy_api_stats/numpy_visitor.py
+++ api_stats/numpy_api_stats/numpy_visitor.py
@@ -2,41 +2,15 @@ import ast
 import types
 import inspect
 from textwrap import dedent
-from collections import deque
-
-# from features import *
-
-NUMPY_API = set()
-
-
-def _compile_lib_matcher(libs):
-    matches = []
-    for allowed in libs:
-        matches.append(allowed.replace('.', '\\.')
-                              .replace('*', '.*$'))
-    return r'|'.join(matches)
-
-
-# ------------------------------------------------------------------------
-# AST Traversal
-# ------------------------------------------------------------------------
-
-GLOBAL = 0
 
 
 class NumpyVisitor(ast.NodeVisitor):
 
-    def __init__(self, features, libs):
-        self.scope = deque([('global', 0)])
-        self.features = features
+    def __init__(self):
+
         self.imports = []
         self.importfroms = []
         self.calls = []
-
-        if libs:
-            self.libs = _compile_lib_matcher(libs)
-        else:
-            self.libs = None
 
     def __call__(self, source):
         if isinstance(source, types.ModuleType):
@@ -81,6 +55,6 @@ class NumpyVisitor(ast.NodeVisitor):
 
     def get_imports(self):
         return [n.name for n in self.imports]
-    
+
     def get_importfroms(self):
         return [n.name for n in self.importfroms]
--- api_stats/numpy_api_stats/test_visitor.py
+++ api_stats/numpy_api_stats/test_visitor.py
@@ -1,17 +1,11 @@
-
-from numpy_api_stats import numpy_visitor as nv      
-
-import sys
-sys.path.insert(0,'/Users/mpeaton/SCIPY')
-
+from numpy_api_stats import numpy_visitor as nv
 from scipy.linalg import basic
 
-
-V = nv.NumpyVisitor(None,None)
+V = nv.NumpyVisitor()
 
 V(basic)
-print(V.get_calls())           
 
+print(V.get_calls())
 print(V.imports)
 print(V.importfroms)
 print(V.get_imports())
